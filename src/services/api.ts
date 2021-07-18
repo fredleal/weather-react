317d70d21ee71fbc754a85f78091797798b5b2b7
fredleal <flm_@msn.com>
2021-07-18 17:41:54 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://goweather.herouapp.com/weather"
+  // baseURL: 'https://www.metaweather.com/api/',
+  baseURL: 'https://goweather.herokuapp.com/weather/',
 })
